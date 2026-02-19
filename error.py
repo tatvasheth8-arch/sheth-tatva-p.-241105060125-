8fc92df7971f0c1c67e36012c9f42dca50567488
tatvasheth8-arch <tatvasheth8@gmail.com>
2026-02-19 00:29:59 -0500

    Add files via upload

---
+++ error.py
@@ -0,0 +1,19 @@
+print("Number Rotation Checker")
+num = int(input("Enter number: "))
+digits = 0
+temp = num
+while temp > 0:
+digits = digits + 1
+temp = temp // 10
+power = 10 ** (digits - 1)
+last = num % 10
+rotated = (last * power) + (num // 10)
+print("Rotated Number:", rotated)
+if rotated == num:
+print("Same number")
+else:
+print("Different number"
+if digits < 2:
+print("Single digit")
+else:
+print("Multiple digits")
